f7e2378b06ed8299ae80b4292d91a870664e86c7
mikaelsoerensen <mikael.soerensen@roskilde-festival.dk>
2017-12-10 14:24:33 +0100

    initial commit

---
+++ assets/config.js
@@ -0,0 +1,39 @@
+// cpanel - site_templates/countdown_fresh/assets/config.js.tt Copyright(c) 2016 cPanel, Inc.
+//                                                          All rights Reserved.
+// copyright@cpanel.net                                        http://cpanel.net
+// This code is subject to the cPanel license. Unauthorized copying is prohibited
+
+window.cpanel = {
+    data: {
+        email: "groentofte@gmail.com",
+        logo: "Grøntofte",
+        social: [
+            
+            {
+                icon: 'facebook',
+                link: "https:\/\/www.facebook.com\/Groentofte\/"
+            },
+            
+            
+            
+            
+            
+            
+        ]
+    },
+    style: {
+        primary: "",
+    },
+    slides: [
+        {
+            type: 'countdown',
+            backgroundImage: "",
+            backgroundColor: "",
+            color: "",
+            buttonText: "",
+            buttonLink: "",
+            endTime: '',
+            content: "",
+        }
+    ]
+};
